40f520c08264bf57eb17286220065946ec9a74c8
isNagatoYuki <computerqym@163.com>
2023-04-13 22:43:21 +0800

    some update

--- src/pages/components/Team.js
+++ src/pages/components/Team.js
@@ -15,6 +15,38 @@ function Team() {
                     <p>Pronouns: She/Her</p>
                     <p>Website: <Link href="https://blog.ctri.cc">blog.ctri.cc</Link></p>
                 </div>
+                <div className={styles.team_member}>
+                    <img src="/0.png" alt="Nagato" />
+                    <h2>Nagato&apos;s Laptop</h2>
+                    <p>Co-Founder/Developer</p>
+                    <p>Programming Languages: Any language that Nagato uses</p>
+                    <p>Pronouns: It/Its</p>
+                    <p>Website: <Link href="https://lenovo.com">lenovo.com</Link></p>
+                </div>
+                <div className={styles.team_member}>
+                    <img src="/1.png" alt="Nagato" />
+                    <h2>Nagato&apos;s Cat #1</h2>
+                    <p>Developer</p>
+                    <p>Programming Languages: Meow</p>
+                    <p>Pronouns: He/His</p>
+                    <p>Website: Meow://MeowMeowMeow.Meow/</p>
+                </div>
+                <div className={styles.team_member}>
+                    <img src="/2.png" alt="Nagato" />
+                    <h2>Nagato&apos;s Cat #2</h2>
+                    <p>Developer</p>
+                    <p>Programming Languages: Meow</p>
+                    <p>Pronouns: She/Her</p>
+                    <p>Website: Meow://MeowMeowMeow.Meow/</p>
+                </div>
+                <div className={styles.team_member}>
+                    <img src="/3.png" alt="Nagato" />
+                    <h2>Nagato&apos;s Cat #3</h2>
+                    <p>Developer</p>
+                    <p>Programming Languages: Meow</p>
+                    <p>Pronouns: He but She</p>
+                    <p>Website: Meow://MeowMeowMeow.Meow/</p>
+                </div>
             </div>
         </div>
     );
